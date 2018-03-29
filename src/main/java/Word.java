1bbb39c480416c0a8732cb96b538102508a99df6
Richard Yan <richardyan314@foxmail.com>
2018-03-28 20:01:12 -0400

    elec 374 - 2017W miniSRC ISA

---
+++ src/main/java/Word.java
@@ -0,0 +1,34 @@
+public class Word
+{
+  private int word = 0;
+  
+  public void op(int op)
+  {
+    this.word |= op << 27;
+  }
+  
+  public void ra(int ra)
+  {
+    this.word |= ra << 23;
+  }
+  
+  public void rb(int rb)
+  {
+    this.word |= rb << 19;
+  }
+  
+  public void rc(int rc)
+  {
+    this.word |= rc << 15;
+  }
+  
+  public void imm(int c)
+  {
+    this.word |= c & 0x7FFFF;
+  }
+  
+  public int get()
+  {
+    return this.word;
+  }
+}
